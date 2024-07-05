08e93481e05c911b24689f36ddd422afc92771b4
Ibrahim Nasr <42195782+inasribrahim@users.noreply.github.com>
2024-07-05 21:11:29 +0300

    Add Automtion Script

---
+++ src/test/java/Academy/BrowserTest.java
@@ -0,0 +1,27 @@
+package Academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://qatesting.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("ibrahim Nasr"));
+		driver.close();
+
+	}
+}
